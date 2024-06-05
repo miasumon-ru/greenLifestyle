732d230bcd630bc369e73de6df8144f9410c6610
miasumon <miasumon.ru@gmail.com>
2024-06-05 15:17:34 +0600

    added navbar component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from "react-helmet-async";
+
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <div>
+                <Helmet>
+                    <title>Home</title>
+                </Helmet>
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
