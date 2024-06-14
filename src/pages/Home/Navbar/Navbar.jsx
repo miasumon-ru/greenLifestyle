7e6099627624948174278880bd51d2d7efd15ddb
miasumon <miasumon.ru@gmail.com>
2024-06-14 12:08:03 +0600

    login button conditional and design of dropdown completed

--- src/pages/Home/Navbar/Navbar.jsx
+++ src/pages/Home/Navbar/Navbar.jsx
@@ -32,8 +32,10 @@ const Navbar = () => {
     const links = <>
 
         <li className="roboto"> <NavLink to='/' className={({ isActive }) => isActive ? ' text-[#23BE0A] border border-[#23BE0A] ' : ''} > Home </NavLink> </li>
+
         <li className="roboto"> <NavLink to='/apartment' className={({ isActive }) => isActive ? ' text-[#23BE0A] border border-[#23BE0A]' : ''} > Apartment </NavLink> </li>
-        <li className="roboto"> <NavLink to='/login' className={({ isActive }) => isActive ? ' text-[#23BE0A] border border-[#23BE0A]' : ''} > Login </NavLink> </li>
+
+        {/* <li className="roboto"> <NavLink to='/login' className={({ isActive }) => isActive ? ' text-[#23BE0A] border border-[#23BE0A]' : ''} > Login </NavLink> </li> */}
 
         {/* <li> <NavLink to='/newRelease' className={({ isActive }) => isActive ? ' text-[#23BE0A] border border-[#23BE0A]' : ''} > New Release </NavLink> </li>
 
@@ -51,7 +53,7 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu  menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu  menu-sm dropdown-content mt-3 z-[50] p-2 shadow bg-base-100 rounded-box w-52">
                         {links}
                     </ul>
                 </div>
@@ -83,23 +85,39 @@ const Navbar = () => {
 
                         </div>
                     </div>
-                    <ul tabIndex={0} className="mt-3 z-[50] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                        <li className="border p-2 bg-gray-400 text-white ">
+                    <ul tabIndex={0} className="mt-3 z-[50] roboto space-y-1 p-2  menu menu-sm dropdown-content bg-base-100 rounded-md w-52">
 
-                            {user?.displayName}
+                        {
+                            user && <li className="flex items-center px-4 py-2  transition-colors duration-300 transform bg-green-200 rounded-md  hover:bg-green-300   hover:text-gray-700" >
 
-                        </li>
-                        <li className="">
+                                {user?.displayName}
 
-                            <Link className="text-center border p-2 bg-gray-400 text-white uppercase" to={'/dashboard/myProfile'}> Dashboard </Link>
+                            </li>
+                        }
 
-
-
-                        </li>
-
+                        {
+                            user &&
 
 
-                        <li className="btn uppercase mt-2" onClick={handleLogOut} > Logout </li>
+
+                            <Link className="" to={'/dashboard/myProfile'}>
+                                <li className="flex items-center px-4 py-2  transition-colors duration-300 transform bg-green-200 rounded-md  hover:bg-green-300   hover:text-gray-700"> Dashboard </li>
+
+                            </Link>
+                        }
+
+
+                        {
+                            user ? <button className="flex items-center justify-center px-4 py-2   transition-colors duration-300 transform bg-green-200 rounded-md  hover:bg-green-300   hover:text-gray-700" onClick={handleLogOut} > Logout </button> :
+
+                                <Link to={'/login'} > <li className="flex text-center items-center px-4 py-2   transition-colors duration-300 transform bg-green-200 rounded-md  hover:bg-green-300   hover:text-gray-700" > Login  </li> </Link>
+
+
+
+                        }
+
+
+
                     </ul>
                 </div>
 
