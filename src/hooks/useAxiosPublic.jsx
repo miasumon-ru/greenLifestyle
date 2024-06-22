28d2834c1c111c58495c0ec76bc6be804990b4c1
miasumon <miasumon.ru@gmail.com>
2024-06-22 08:40:01 +0600

    data theme light and other necessary things for hosting added

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
 
-    baseURL : "http://localhost:5000"
+    baseURL : "https://assignment-twelve-server-side-theta.vercel.app"
 })
 
 const useAxiosPublic = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -9,7 +9,7 @@ import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
 
-    baseURL:"http://localhost:5000"
+    baseURL:"https://assignment-twelve-server-side-theta.vercel.app"
 
 })
 
